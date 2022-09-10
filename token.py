00dc3b4f02cd6d05eff37c4e103874b634c1186a
therealkujo <106406727+therealkujo@users.noreply.github.com>
2022-09-09 18:23:36 -0700

    change variable

--- token.py
+++ token.py
@@ -1,5 +1,5 @@
 from github import Github, GithubIntegration
 import os
 
-private_key = os.environ['SUPER_SECRET_PEM']
+private_key = os.environ['SUPER_SECRET_CERT']
 print(GithubIntegration(236743, private_key).get_access_token(29073911))
